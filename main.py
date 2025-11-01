f6481810f792d6ff491334b497875b8ea2b18fa2
Marcossxp <marcosaurelio2417@gmail.com>
2025-11-01 17:17:12 -0400

    concluido

--- main.py
+++ main.py
@@ -3,3 +3,20 @@ import pandas as pd
 
 st.title("Oficina OpenDay ADS")
 
+# Chama a view lista_musicas
+from view.lista_musicas import lista_musicas_view
+
+st.set_page_config(
+    page_title="lista music",
+    page_icon="🎸",
+    layout="wide",
+)
+
+
+def main():
+    st.title("App Principal")
+    lista_musicas_view()
+    
+
+if __name__ == "__main__":
+    main()
